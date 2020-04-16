3ed2e46beb457a49941c0a80cc9a167af8bc3fe9
@willhead <60652390+will-head@users.noreply.github.com>
2020-04-16 16:01:05 +0100

    DockingStation returns working bike, passes tests

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,4 +1,13 @@
+# Set $debug to true to enable command line output, disable for rspec
+# $debug = true
+
+# require './lib/bike.rb'
+require_relative 'bike'
+
 class DockingStation
   def release_bike
+    bike = Bike.new
   end
 end
+docking_station = DockingStation.new if $debug
+docking_station.release_bike if $debug
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,10 @@
 require 'docking_station'
 describe DockingStation do
   it {is_expected.to respond_to :release_bike}
+  it 'is of class Bike' do
+    expect(subject.release_bike).to be_instance_of Bike
+  end
+  it 'checks bike is working' do
+    expect(subject.release_bike.working?).to eq true
+  end
 end
